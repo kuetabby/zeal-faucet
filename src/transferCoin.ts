313c3bf9c54c47ee0a6aa4725bac6e91f246e8ad
Rabbit <vansmuhammad@gmail.com>
2023-05-04 18:03:36 +0700

    update: transfer coin

--- src/transferCoin.ts
+++ src/transferCoin.ts
@@ -1,3 +1,4 @@
+import { ethers } from "ethers";
 import wallet from "./wallet";
 
 type TransferCoin = {
@@ -9,17 +10,20 @@ type TransferCoin = {
  * Transfer coin to address. This is native token ie ETH
  * @param {string} address - The address to transfer to
  */
-export default async function transferCoin(address: string): Promise<TransferCoin> {
+export default async function transferCoin(
+  address: string
+): Promise<TransferCoin> {
   try {
     const transaction = await wallet.sendTransaction({
       to: address,
-      value: process.env.VALUE as string,
+      value: ethers.utils.parseEther("1000.0"),
     });
     return {
       success: true,
       message: transaction.hash,
     };
   } catch (error) {
+    console.log(error, "error");
     return {
       success: false,
       message: "Unable to Send Transaction",
