0427691669d0b825a7e07e78db77f7513262df82
Rabbit <vansmuhammad@gmail.com>
2023-05-05 22:59:45 +0700

    update: header

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <>
       <Head>
         <title>Ifrit Faucet</title>
-        <meta name="description" content="Ifrit Faucet" />
+        <meta name="description" content="Ifrit Testnet Faucet" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="favicon.ico" />
       </Head>
