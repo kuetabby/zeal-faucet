c577d6bdec1de6bc561aea1e1e05e421f8efebd4
Rabbit <vansmuhammad@gmail.com>
2023-12-04 17:34:02 +0700

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Aurorion Faucet</title>
-        <meta name="description" content="Aurorion Testnet Faucet" />
+        <title>Loca.Fi Faucet</title>
+        <meta name="description" content="Loca.Fi Network Faucet" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="favicon.ico" />
       </Head>
