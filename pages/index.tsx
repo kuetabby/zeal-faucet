b041da533902f2783ed8817567521fab8f352863
kuetabby <vansmuhammad@gmail.com>
2024-02-24 02:29:03 +0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Avernus Faucet</title>
-        <meta name="description" content="Avernus Faucet" />
+        <title>Zeal-AI Faucet</title>
+        <meta name="description" content="Zeal Faucet" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="favicon.ico" />
       </Head>
